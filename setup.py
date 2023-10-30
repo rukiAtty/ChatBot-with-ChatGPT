08382e01c0c6d77d54da511fa0b1bc4d6193c4f6
rukiAtty <rukshanaattygalle@gmail.com>
2023-10-30 11:23:30 +0530

    first commit from vs code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a custom chatbot train for a given website with chatGPT API',
+    author='Rukshana Attygalle',
+    license='MIT',
+)
